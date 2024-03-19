8e644a2fb499e71eba7a233bb748dc6ec2ae10d5
MursaleenShah <shahmursalin9@gmail.com>
2024-03-19 07:17:57 +0530

    Added pageNumbers to be extracted in pdf Schema

--- pdfDetails.js
+++ pdfDetails.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 //creating a schema for pdf
 const PdfDetailsSchema = new mongoose.Schema({
     pdf:String,
-    title:String
+    title:String,
+    pageNumbers:String,
 });
 //creating a model for schema
 const PdfDetails = mongoose.model("PdfDetails",PdfDetailsSchema);
